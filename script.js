29bf799e447bb0831568d39f460f6d1aeb37cf92
GURUPREET6365 <kumargurupreet2008@gmail.com>
2025-09-14 00:52:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// Creating two container for storing the score of the user.
+let userScore = 0;
+let computerScore = 0;
+
+// computer choice generating
+const computerChoice = () => {
+  let options = ["rock", "paper", "scissor"];
+  let randomChoice = Math.floor(Math.random() * 3);
+  return options[randomChoice];
+}; 
+// Function for DrawGame
+const drawGame = () => {
+  console.log("Game is Draw");
+  message.innerText = "Game is Draw";
+  message.style.backgroundColor = "gray";
+};
+
+// Creating a function for showing the winner of the game.
+const showWinner = (userWin, userchoice, compChoice) => {
+  if (userWin) {
+    console.log("You Win the Game");
+    userScore++;
+    userScorePara.innerText = userScore;
+    message.innerText = `You Win! ${userchoice} beats ${compChoice}`;
+    message.style.backgroundColor = "green";
+  } else {
+    console.log("Computer Wins the Game");
+    computerScore++;
+    computerScorePara.innerText = computerScore;
+    message.innerText = `You Lose! ${compChoice} beats ${userchoice}`;
+    message.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userchoice) => {
+  console.log("user choice is", userchoice);
+  const compChoice = computerChoice();
+  console.log("computer choice is", compChoice);
+
+  if (userchoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userchoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userchoice === "paper") {
+      userWin = compChoice === "scissor" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userchoice, compChoice);
+  }
+};
+
+// Accessing the user score span for showing the score of the user.
+const userScorePara = document.querySelector("#you");
+
+// Accessing the computer score span for showing the score of the computer.
+const computerScorePara = document.querySelector("#computer");
+
+// Accessing the message div for showing the result of the game.
+const message = document.querySelector("#message");
+
+// Accessing all the choices for making the score.
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choice) => {
+  // Accessing Id
+
+  choice.addEventListener("click", () => {
+    const choiceId = choice.getAttribute("id");
+    playGame(choiceId);
+  });
+});
